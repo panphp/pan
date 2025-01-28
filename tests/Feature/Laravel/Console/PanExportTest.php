af9b958c90dce1747da6cb696eb3315aa4056798
skdishansachin <skdishansachin@gmail.com>
2025-01-28 23:00:42 +0530

    Add test for exporting all analytics in PanExportCommand

--- tests/Feature/Laravel/Console/PanExportTest.php
+++ tests/Feature/Laravel/Console/PanExportTest.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Storage;
 use Pan\Contracts\AnalyticsRepository;
 use Pan\Enums\EventType;
 
@@ -10,4 +11,21 @@ it('displays a message if there are no analytics', function (): void {
     $response
         ->expectsOutputToContain('There are no analytics to export.')
         ->assertExitCode(0);
+});
+
+it('exports all analytics', function (): void {
+    $analytics = app(AnalyticsRepository::class);
+
+    $analytics->increment('dashboard', EventType::IMPRESSION);
+    $analytics->increment('dashboard', EventType::IMPRESSION);
+    $analytics->increment('dashboard', EventType::IMPRESSION);
+
+    $analytics->increment('dashboard', EventType::HOVER);
+    $analytics->increment('dashboard', EventType::HOVER);
+
+    $analytics->increment('dashboard', EventType::CLICK);
+
+    $exitCode = Artisan::call('pan:export');
+
+    expect($exitCode)->toBe(0);
 });
